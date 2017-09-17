6b53f4823a4cf45e8d3daa4b2d38b468f3941ddc
TortleWortle <bas.kuipers@me.com>
2017-09-17 22:39:27 +0200

    idk bruh

--- modules/radioplay.js
+++ modules/radioplay.js
@@ -1,8 +1,10 @@
 const https = require('https');
 const request = require('request')
 const stream = require('stream')
+const ytdl = require('ytdl-core')
 const dispatchers = []
 const connections = []
+const streams = []
 
 
 
@@ -65,20 +67,59 @@ module.exports = (bot) => {
 		}, 5 * 1000)
 	})
 
-	bot.addCommand('play', (payload) => {
-		var message = payload.message
+	bot.addCommand('play( *site/watch?v=:video)', (payload) => {
+		let message = payload.message
 		let id = message.guild.id
-		let voiceChannel = message.member.voiceChannel
+		var video = payload.params.video
+		var site = payload.params.site
+		console.log(payload.params)
+
+		if(site != null && video != null) {
+			if(site.includes("youtube.com") || site.includes("youtu.be") || true) {
+				var voice = getVoiceConnection(message)
+				if(voice != null) {
+					voice.then(connection => {
+						message.guild.id
+						streams[message.guild.id] = ytdl("https://www.youtube.com/watch?v=" + video)
+			        	connection.playStream(streams[message.guild.id])
+
+			        	streams[message.guild.id].on("end", () => {
+			        		streams[message.guild.id] = null
+			        		connection.playBroadcast(broadcast)
+			        	})
+
+			        	streams[message.guild.id].on("error", (error) => {
+			        		streams[message.guild.id] = null
+			        		message.reply(error.message)
+			        		connection.playBroadcast(broadcast)
+			        	})
+			        })
+		        }
+			}
+		}else {
+			console.log("playing normal")
+			var voice = getVoiceConnection(message)
+			if(voice != null) {
+				if(streams[message.guild.id] != null) {
+					streams[message.guild.id].destroy()
+				}
+				voice.then(connection => {
+					console.log("playing broadcast")
+		        	setTimeout(() => {
+		        		connection.playBroadcast(broadcast)
+		        	}, 300)
+		        })
+	        }
+		}
             
-        if (!message.guild.voiceConnection) {
-            if (!message.member.voiceChannel) return message.channel.sendMessage('You need to be in a voice channel')
-			voiceChannel.join()
-				.then(connection => {
-	   				connection.playBroadcast(broadcast)            
-				})
-        }
+
 	});
 
+	function getVoiceConnection(message) {
+		let voiceChannel = message.member.voiceChannel
+        if (!message.member.voiceChannel) return message.channel.sendMessage('You need to be in a voice channel')
+		return voiceChannel.join()
+	}
 
 	bot.addCommand('stop', (payload) => {
 		var message = payload.message
